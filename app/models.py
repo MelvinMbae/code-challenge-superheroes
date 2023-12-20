4b7b49e85d370404ae4afc6d3550ee6f76bc21a9
MelvinMbae <melvinmuthomi95@gmail.com>
2023-12-21 02:39:38 +0300

    seed data done

--- app/models.py
+++ app/models.py
@@ -8,10 +8,10 @@ class Hero(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String)
     super_name=db.Column(db.String)
-    created_at=db.Column(db.DateTime, server_default=db.func.now)
-    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
     
-    powers=db.relationship("Power")
+    powers=db.relationship("HeroPower")
     
     def __repr__(self):
         return f"{self.name} with a super power name of {self.super_name}"
@@ -22,8 +22,8 @@ class Power(db.Model, SerializerMixin):
     id= db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String)
     description=db.Column(db.String)
-    created_at=db.Column(db.DateTime, server_default=db.func.now)
-    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
     
     def __repr__(self):
         return f"{self.name} super power can be described as someone that has the power of {self.description}"
@@ -31,13 +31,13 @@ class Power(db.Model, SerializerMixin):
 class HeroPower(db.Model, SerializerMixin):
     __tablename__='hero_powers'
     
-    id=db.Column(db.Integer)
+    id=db.Column(db.Integer, primary_key=True)
     strength=db.Column(db.String)
-    power_id=db.Column(db.Integer, db.ForeignKey("power_id"))
-    hero_id=db.Column(db.Integer, db.ForeignKey("hero_id"))
-    created_at=db.Column(db.DateTime, server_default=db.func.now)
-    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
+    power_id=db.Column(db.Integer, db.ForeignKey("powers.id"))
+    hero_id=db.Column(db.Integer, db.ForeignKey("heroes.id"))
+    created_at=db.Column(db.DateTime, server_default=db.func.now())
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f"{self.strength} belongs to super hero ID: {self.hero_id}"
+        return f"{self.strength} belongs to super hero with ID: {self.hero_id}"
 # add any models you may need.
---
+++ app/seed.py
@@ -0,0 +1,48 @@
+from faker import Faker
+from models import Hero, Power, HeroPower,db
+import random
+from app import app 
+
+fake = Faker()
+
+with app.app_context():
+
+    db.session.query(Hero).delete()
+    db.session.query(Power).delete()
+    db.session.query(HeroPower).delete()
+
+    heroes = []
+    for _ in range(10):  
+        hero = Hero(
+            name=fake.name(),
+            super_name=fake.word()
+        )
+        heroes.append(hero)
+
+    db.session.add_all(heroes)
+    db.session.commit()
+
+    powers = []
+    for _ in range(10):
+        power = Power(
+            name=fake.word(),
+            description=fake.sentence(nb_words=6)
+        )
+        powers.append(power)
+
+    db.session.add_all(powers)
+    db.session.commit()
+
+    # Generate associations (HeroPower)
+    hero_powers = []
+    elements=['Strong', 'Weak', 'Average']
+    for _ in range(20):
+        hero_power = HeroPower(
+            strength=random.choice(elements),
+            hero_id=random.choice(heroes).id,
+            power_id=random.choice(powers).id
+        )
+        hero_powers.append(hero_power)
+
+    db.session.add_all(hero_powers)
+    db.session.commit()
