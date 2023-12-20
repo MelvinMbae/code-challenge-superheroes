1201db85fa8d6a3db198f6c85a588c8c2f23c2a2
MelvinMbae <melvinmuthomi95@gmail.com>
2023-12-21 01:50:38 +0300

    models created

--- app/models.py
+++ app/models.py
@@ -1,10 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    super_name=db.Column(db.String)
+    created_at=db.Column(db.DateTime, server_default=db.func.now)
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
+    
+    powers=db.relationship("Power")
+    
+    def __repr__(self):
+        return f"{self.name} with a super power name of {self.super_name}"
+    
+class Power(db.Model, SerializerMixin):
+    __tablename__='powers'
+    
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    description=db.Column(db.String)
+    created_at=db.Column(db.DateTime, server_default=db.func.now)
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
+    
+    def __repr__(self):
+        return f"{self.name} super power can be described as someone that has the power of {self.description}"
+    
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__='hero_powers'
+    
+    id=db.Column(db.Integer)
+    strength=db.Column(db.String)
+    power_id=db.Column(db.Integer, db.ForeignKey("power_id"))
+    hero_id=db.Column(db.Integer, db.ForeignKey("hero_id"))
+    created_at=db.Column(db.DateTime, server_default=db.func.now)
+    updated_at=db.Column(db.DateTime, onupdate=db.func.now)
 
-# add any models you may need. 
+    def __repr__(self):
+        return f"{self.strength} belongs to super hero ID: {self.hero_id}"
+# add any models you may need.
